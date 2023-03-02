1b364e675ddf6efd6cee209f365bba25335ba7ab
Martin <panovski.martin93@gmail.com>
2023-03-02 09:10:29 +0100

    Class exercises solutions added

--- G1/Class02/SEDC.Class02/SEDC.Class02.VariabalesAndDataTypes/Program.cs
+++ G1/Class02/SEDC.Class02/SEDC.Class02.VariabalesAndDataTypes/Program.cs
@@ -122,8 +122,7 @@ bool userInput5 = int.TryParse(Console.ReadLine(), out int resultNumber);
 
 #endregion
 
-
-
+Console.WriteLine("=====================================");
 
 #region Branching (if/else and switch)
 
@@ -143,7 +142,7 @@ int userNum1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Please enter number");
 int userNum2 = int.Parse(Console.ReadLine());
 
-if(userNum1 != 0 && userNum2 != 0)
+if (userNum1 != 0 && userNum2 != 0)
 {
     if (userNum1 > userNum2)
     {
@@ -227,6 +226,141 @@ switch (userInputDay.ToLower())
 
 #endregion
 
+
+#region Class Exercises Solutions
+
+// Exercise 1
+
+double firstNumber = 10;
+double secondNumber = 22.9;
+
+double resultDivide = firstNumber / secondNumber;
+Console.WriteLine(resultDivide);
+
+
+int firstNum = 44;
+int secondNum = 34;
+
+double res = firstNumber / secondNumber;
+Console.WriteLine(res);
+
+
+// Exercise 2
+
+string fName = "Filip ";
+string lName = "Petkov";
+string resultName = fName + lName;
+Console.WriteLine(resultName);
+
+
+string numAsString1 = "9";
+string numAsString2 = "3";
+string numAsStringResult = numAsString1 + numAsString2;
+Console.WriteLine(numAsStringResult);
+
+
+
+// Exercise 3
+// Using the previously declared variables I am directly jumping on the result variable
+
+// REMINDER: In order to use '+' sign for variable of type string and integer, the result variable 
+//           must be of type string!!!
+
+string mixedResult = fName + numAsString1;
+Console.WriteLine(mixedResult);
+
+
+// Exercise 4
+int mobileCredit = 102;
+int singleMessagePrice = 5;
+double maxNumberOfMessagesSent = mobileCredit / singleMessagePrice;
+Console.WriteLine(maxNumberOfMessagesSent);
+
+
+// Exercise 5
+int numOfBranches = 12;
+int numOfApplesOnBranch = 8;
+double basketCapacity = 5;
+
+Console.WriteLine("Please insert number of trees:");
+bool isUserInputParsed = int.TryParse(Console.ReadLine(), out int numOfTrees);
+if(isUserInputParsed)
+{
+    int totalBranches = numOfBranches * numOfTrees;
+    double totalApples = totalBranches * numOfApplesOnBranch;
+    double basketNumber = totalApples / basketCapacity;
+    if(basketNumber % 2 != 0)
+    {
+        // Math.Ceiling() is a method so that if a decimal number is passed as parameter,
+        // it will always return the next whole number as result
+        basketNumber = Math.Ceiling(basketNumber); 
+    }
+    Console.WriteLine("When you have " + numOfTrees + 
+                      " with total of " + totalApples + 
+                      " apples on them, you need " + basketNumber + 
+                      " to collect all the apples");
+}
+
+// Exercise 6
+Console.WriteLine("Please enter whole number: ");
+bool canFirstBeparsed = int.TryParse(Console.ReadLine(), out int numberInput1);
+Console.WriteLine("Please enter whole number: ");
+bool canSecondBeparsed = int.TryParse(Console.ReadLine(), out int numberInput2);
+
+if(canFirstBeparsed && canSecondBeparsed)
+{
+    // Greater than check
+    if(numberInput1 > numberInput2)
+    {
+        if(number1 % 2 == 0)
+        {
+            Console.WriteLine(numberInput1 + " is greater than " + numberInput2 + " and it is an Even number");
+        }
+        else
+        {
+            Console.WriteLine(numberInput1 + " is greater than " + numberInput2 + " and it is an Odd number");
+        }
+    }
+    else if(numberInput1 < numberInput2)
+    {
+        if (number1 % 2 == 0)
+        {
+            Console.WriteLine(numberInput2 + " is greater than " + numberInput1 + " and it is an Even number");
+        }
+        else
+        {
+            Console.WriteLine(numberInput2 + " is greater than " + numberInput1 + " and it is an Odd number");
+        }
+        
+    }
+    else
+    {
+        Console.WriteLine("The numbers are equal!");
+    }
+}
+
+
+// Exercise 5
+bool canBeParsed = int.TryParse(Console.ReadLine(), out int userNumberInput);
+if(canBeParsed)
+{
+    switch (userNumberInput)
+    {
+        case 1:
+            Console.WriteLine("You got a new car!");
+            break;
+        case 2:
+            Console.WriteLine("You got a new plane!");
+            break;
+        case 3:
+            Console.WriteLine("You got a new bike!");
+            break;
+        default:
+            Console.WriteLine("Oops! Something went wrong!");
+            break;
+    }
+}
+#endregion
 
 
 Console.ReadLine();
