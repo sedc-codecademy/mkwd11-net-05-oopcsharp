03a613830cea1ff312a551a36007a27f9cfec165
tstojanovska <stojanovskatanja03@gmail.com>
2023-03-07 20:50:51 +0100

    Class 04

---
+++ G6/Class04/Code/RetroExercises/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] ourArray = new int[5];
+
+for(int i=0; i < ourArray.Length; i++)
+{
+    Console.WriteLine("Enter value for index: " + i);
+    //string input = Console.ReadLine();
+    //int number = int.Parse(input);
+
+    int number = int.Parse(Console.ReadLine());
+    ourArray[i] = number;
+}
+
+
+int sum = 0;
+foreach(int num in ourArray)
+{
+    sum += num;
+}
+
+Console.WriteLine("The sum is: " + sum);
---
+++ G6/Class04/Homework.md
@@ -0,0 +1,13 @@
+# Homework Class 4 📒
+
+## Task 1
+* Take one string from the input and print its last 5 characters.
+
+## Task 2
+* Take a sentence as input and print its words.
+
+## Task 3
+* Create a function that takes a number as input. This method should return the sum of the digits in the number.
+
+##Bonus
+Take a sentence as input and print its largest word.
