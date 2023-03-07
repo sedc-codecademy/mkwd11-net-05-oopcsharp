914476d4c5bc58d4a6fadc83c34b95df2b6cd6bb
petreArsovski <email@example.comarso.pet@outlook.com>
2023-03-07 21:16:48 +0100

    class 01 code

---
+++ G2/Class01/ClassCode/OurFirstSolution/MyNewProject/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name = "Dana";
+Console.WriteLine(name);
+
+int number = 10;
+Console.WriteLine(number);
+
+Console.ReadLine();
+
+string someString = "string example";
+Console.WriteLine(someString);
+
+string someString2 = "new string example";
+Console.Write(someString2);
+
+Console.ReadLine();
