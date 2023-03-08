14c30fe96c1671fab66fa9886c5e9e13e2c2dd0e
trajanstevkovski <trajanstevkovski@gmail.com>
2023-03-08 20:35:10 +0100

    Class05 ClassesAndObjects

---
+++ G3/Class05/SEDC.Class05/SEDC.Class05.ClassesAndObjects/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Class05.ClassesAndObjects
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+
+        public void Buy(double money)
+        {
+            if(money >= Price)
+            {
+                Console.WriteLine($"You just bought a {Name}");
+            }
+            else 
+            {
+                Console.WriteLine($"You need {Price - money} more to buy this item!");
+            }
+        }
+    }
+}
---
+++ G3/Class05/SEDC.Class05/SEDC.Class05.ClassesAndObjects/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SEDC.Class05.ClassesAndObjects
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public void Talk()
+        {
+            Console.WriteLine("Talking!!");
+        }
+    }
+}
