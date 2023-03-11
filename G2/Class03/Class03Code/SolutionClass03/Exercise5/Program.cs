bae3b4eb9e9408b3a8925c106282403d630851d3
petreArsovski <email@example.comarso.pet@outlook.com>
2023-03-11 12:55:37 +0100

    Class 03

---
+++ G2/Class03/Class03Code/SolutionClass03/Exercise5/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] words = { "Hello", "Hi", "Wow", "Words", "Dog" };
+double[] decimals = new double[] { 2.4, 34.5, 3.54, 65.23, 123.3 };
+char[] chars = { 'a', '*', '&', 'd', 'e' };
+bool[] bools = new bool[] { true, false, false, false, false };
+int[][] arrayOfInts = new int[][]
+{
+    new int[] {2, 5},
+    new int[] {6, 5},
+    new int[] {6, 21},
+    new int[] {9, 1},
+    new int[] {10, 1}
+};
+
+Console.WriteLine("Words:");
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+foreach(int[] arrayInteger in arrayOfInts)
+{
+    foreach(int integer in arrayInteger)
+    {
+        Console.WriteLine(integer);
+    }
+}
+ 
---
+++ G2/Class03/Class03Code/SolutionClass03/Loops/Program.cs
@@ -0,0 +1,94 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("===For loop===");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+//for(; ; )
+//{
+//    Console.WriteLine("Hello");
+//}
+
+for(; ; )
+{
+    Console.WriteLine("Enter input");
+    string input = Console.ReadLine();
+
+    //if (input == "X" || input == "x")
+    if(input.ToLower() == "x")
+    {
+        break;
+    }
+}
+
+for(int i = 0; i < 10; i++)
+{
+    if(i == 5)
+    {
+        Console.WriteLine("Breaking...");
+        break;
+    }
+
+    if(i == 3)
+    {
+        Console.WriteLine("Skipping...");
+        continue;
+    }
+}
+
+Console.WriteLine("===while===");
+
+int counter = 0;
+
+while(counter <= 10)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
+
+while (true)
+{
+    Console.WriteLine("Enter inpup");
+    string input = Console.ReadLine();
+
+    if(input.ToLower() == "yes")
+    {
+        break;
+    }
+}
+
+int anotherCounter = 0;
+while(anotherCounter <= 10)
+{
+    if (anotherCounter == 3)
+    {
+        Console.WriteLine("Skipping in while...");
+        anotherCounter++;
+        continue;
+    }
+    if (anotherCounter == 5)
+    {
+        Console.WriteLine("Breaking in while..");
+        break;
+    }
+    Console.WriteLine(anotherCounter);
+    anotherCounter++;
+}
+
+Console.WriteLine("===Do while");
+int thirdCounter = 0;
+do
+{
+    Console.WriteLine(thirdCounter);
+    thirdCounter++;
+}
+while(thirdCounter <= 3);
+
+do
+{
+    Console.WriteLine("This message will be written!");
+}
+while (false);
