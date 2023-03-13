6b4ce2c01609cc1261efa5e0e90e5f76bf1fb666
Martin <panovski.martin93@gmail.com>
2023-03-13 20:55:08 +0100

    Class 05
    Class 05 demo code added

--- G1/Class04/SEDC.Class04/SEDC.DateTime/Program.cs
+++ G1/Class04/SEDC.Class04/SEDC.DateTime/Program.cs
@@ -18,3 +18,43 @@ Console.WriteLine(currentDate.Date);
 DateTime newDate = new DateTime(1993, 08, 27);
 Console.WriteLine(newDate);
 
+
+// Add and subtract years 
+DateTime currentDatePlusTwoYears = currentDay.AddYears(2);
+//DateTime currentDateMinusTwoYears = currentDay.AddYears(-2).AddMonths(-2).AddDays(1).AddMinutes(2);
+
+
+// Instead of creating multiple variables you can chain the methods like on the line 24 above
+DateTime currentDateMinusTwoYears = currentDay.AddYears(-2);
+
+DateTime currentDateMinusTwoYearsMinusTwoMonths = currentDateMinusTwoYears.AddMonths(-2);
+
+DateTime currentDateMinusTwoYearsMinusTwoMonthsMinusTwoDays = currentDateMinusTwoYearsMinusTwoMonths.AddDays(-2);
+
+
+
+Console.WriteLine(currentDatePlusTwoYears);
+Console.WriteLine(currentDateMinusTwoYears);
+
+
+
+// Parsing string into dates
+
+string myBirthDay = "08/27/1993";
+DateTime myBirthDate = DateTime.Parse(myBirthDay);
+Console.WriteLine("I am born on " + myBirthDate);
+
+
+string myBirthDateFormated = myBirthDate.ToString("dd/MM/yyyy");
+string myBirthDateFormated1 = myBirthDate.ToString("dddd MMM yy");
+string myBirthDateAndHour = myBirthDate.ToString("MM-dd-yyyy HH:mm:ss");
+
+
+string formattedDate = string.Format("{0:dd-MMM-yyyy}", myBirthDate);
+
+Console.WriteLine(myBirthDateFormated);
+Console.WriteLine(myBirthDateFormated1);
+Console.WriteLine(myBirthDateAndHour);
+Console.WriteLine(formattedDate);
+
+
---
+++ G1/Class05/SEDC.Class05/SEDC.OOPClasses_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using SEDC.OOPClasses_1.Models;
+
+
+// Create an object by using the default constructor
+Person person = new Person();
+person.FirstName = "Martin";
+person.LastName = "Panovski";
+
+
+DateTime birthDate = DateTime.Parse("08/27/1993");
+
+person.GetYearsOldInfo(birthDate);
+
+person.SetPhoneNumber("070/222-333");
+Console.WriteLine(person.GetPhoneNumber());
+Console.WriteLine(person.FirstName);
+
+
+// Create an object using constructor with parameters
+Book book = new Book("Harry Potter 1", "J.K. Rowling", "123456789", 300, "Adventure");
+
+Console.WriteLine(book.GetBookInfo());
+
+Book book1 = new Book();
+book1.Title = "Harry Potter 2";
+
+Book book2 = new Book("Harry Potter 3");
+
+
+
+// Create an object by using the objcet initializer
+
+Person person1 = new Person()
+{
+    FirstName = "Antonio",
+    LastName = "Novoselski"
+};
+
+
+Person person2 = new Person
+{
+    FirstName = "Antonio",
+    LastName = "Novoselski"
+};
+
+
+Person person3 = new()
+{
+    FirstName = "Antonio",
+    LastName = "Novoselski"
+};
