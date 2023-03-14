16a932e3c048640fb618a0f5d7bdd88f3b92acfb
tstojanovska <stojanovskatanja03@gmail.com>
2023-03-14 20:55:43 +0100

    Class 06

---
+++ G6/Class06/Code/Class06/Task02/Program.cs
@@ -0,0 +1,39 @@
+﻿void NumberStats(double number)
+{
+    bool isNegative = number < 0;
+    bool isDecimal = number % 1 > 0;
+    //ternary operator
+    string type = isDecimal ? "Decimal" : "Whole number";
+    bool isEven = number % 2 == 0;
+    Console.WriteLine($"Stats for number: {number}");
+    Console.WriteLine($"{number} is negative: {isNegative}.");
+    Console.WriteLine($"{number} is {type}.");
+    Console.WriteLine($"{number} is Even: {isEven}.");
+}
+
+bool flag = true;
+
+
+do
+{
+    Console.Write("Enter a number:");
+    double number;
+    bool isNumber = double.TryParse(Console.ReadLine(), out number);
+    if (!isNumber)
+    {
+        Console.WriteLine("That was not a number! Please try again!");
+    }
+    else
+    {
+        NumberStats(number);
+    }
+
+    Console.Write("Press any key to try again or X to exit.");
+    string input = Console.ReadLine();
+    if (input.Equals("X", StringComparison.InvariantCultureIgnoreCase)) //ignore case (upper, lower) of both strings
+    {
+        flag = false;
+    }
+    flag = true;
+}
+while (flag);
---
+++ G6/Class06/Code/RetroExercise/RetroExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using RetroExercise.Models;
+using System.Globalization;
+
+Student[] students = new Student[5];
+//call the constructor with params from the Student class, to initialize objects
+students[0] = new Student("Tanja", "Stojanovska", "Programming", "G2");
+students[1] = new Student("Stefan", "Trajkov", "Programming", "G4");
+students[2] = new Student("Bojan", "Damchevski", "QA", "G3");
+students[3] = new Student("Jovana", "Jovanovska", "Networks", "G3");
+students[4] = new Student("Nikola", "Nikolovski", "Design", "G7");
+
+Console.WriteLine("Enter first name:");
+string input = Console.ReadLine();
+
+Student foundStudent = FindStudent(students, input);
+
+if(foundStudent != null)
+{
+    Console.WriteLine("We found the student!");
+}
+else
+{
+    Console.WriteLine("Student was not found");
+}
+
+Student FindStudent(Student[] studentsArray, string firstName)
+{
+    foreach(Student student in studentsArray)
+    {
+        if(student.FirstName.ToLower() == firstName.ToLower())
+        {
+            return student; //stop the loop, exit the method
+        }
+    }
+
+    return null;
+}
