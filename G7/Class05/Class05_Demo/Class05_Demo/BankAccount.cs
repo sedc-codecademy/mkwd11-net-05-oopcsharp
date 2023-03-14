bc22ebb654f189449256846725407d1cf742dcd9
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2023-03-14 21:08:26 +0100

    Class 05

---
+++ G7/Class05/Class05_Demo/Class05_Demo/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿namespace Class05_Demo
+{
+    public class BankAccount
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public long AccountNumber { get; set; }
+        private decimal Balance { get; set; }
+        public string Info { get; set; }
+
+        public BankAccount(string firstName, string lastName, string id, long accountNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            AccountNumber = accountNumber;
+            Balance = 0;
+            Info = "Constructed from first constructor";
+        }
+
+        public BankAccount(string firstName, string lastName, string id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Balance = 0;
+            Info = "Constructed from second constructor";
+        }
+
+        public BankAccount()
+        {
+            Balance = 0;
+            Info = "Constructed from Default constructor";
+        }
+
+        public void Withdraw(int amount)
+        {
+            if(Balance < amount)
+            {
+                Console.WriteLine("Limit exceeded!");
+                return;
+            }
+
+            Balance = Balance - amount;
+            //Balance -= amount;
+            Console.WriteLine($"You have successfuly withdrawn {amount:C} money");
+        }
+
+        public void Deposit(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("The amount for the deposit needs to be larger than 0");
+                return;
+            }
+
+            Balance += amount;
+            Console.WriteLine($"You have successfuly deposit {amount:C} money");
+        }
+    }
+}
---
+++ G7/Class05/Class05_Exercise03/Class05_Exercise03/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Class05_Exercise03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Test 1", "Programming", "G7");
+            Student s2 = new Student("Test 2", "QA", "G3");
+            Student s3 = new Student("Test 3", "Programming", "G5");
+            Student s4 = new Student("Test 4", "Networking", "G1");
+            Student s5 = new Student("Test 5", "Design", "G1");
+            Student s6 = new Student("Test 3", "Programming", "G8");
+
+            Student[] students = new Student[6] { s1, s2, s3, s4, s5, s6 };
+
+            Console.WriteLine("Please write the name of the student:");
+            string input = Console.ReadLine();
+
+            bool studentFound = false;
+
+            for(int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Name == input)
+                {
+                    Console.WriteLine($"We have found student: {students[i].Name} that matches the search");
+                    studentFound = true;
+                }
+            }
+
+            
+        }
+    }
+}
