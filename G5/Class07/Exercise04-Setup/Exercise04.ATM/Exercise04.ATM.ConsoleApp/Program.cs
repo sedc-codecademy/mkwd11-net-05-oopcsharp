5975f8dc4b77118634c3a32903a31a64fe148c96
Angel Mitrov <Angel.Mitrov@seavus.com>
2023-03-15 18:02:39 +0100

    Added Exercise04 Setup

---
+++ G5/Class07/Exercise04-Setup/Exercise04.ATM/Exercise04.ATM.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Exercise04.ATM.ConsoleApp.Classes;
+
+Customer[] database = new Customer[]
+{
+    new Customer("Angel", "Mitrov", new Card(1234123412341234, 100)),
+    new Customer("John", "Doe", new Card(4321432143214321, 1000)),
+};
+
+
+foreach (Customer customer in database)
+{
+    customer.GetCustomerInfo();
+}
