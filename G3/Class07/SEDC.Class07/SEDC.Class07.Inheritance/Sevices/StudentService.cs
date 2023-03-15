fa927940f5276f182089b15b23718255283ec2ea
trajanstevkovski <trajanstevkovski@gmail.com>
2023-03-15 19:50:24 +0100

    Class07 ingeritance, enum

---
+++ G3/Class07/SEDC.Class07/SEDC.Class07.Inheritance/Sevices/StudentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Class07.Inheritance.Sevices
+{
+    public class StudentService : BaseInputReader
+    {
+    }
+}
---
+++ G3/Class07/SEDC.Class07/SEDC.Class07.Inheritance/Sevices/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Class07.Inheritance.Sevices
+{
+    public class UserService : BaseInputReader
+    {
+        public void CreateUser()
+        {
+            string firstName = GetStringFromScreen();
+            string lastName = GetStringFromScreen();
+            // rest of the logic
+        }
+    }
+}
