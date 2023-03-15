0ce90e8e12ade0c950a74f827cc995767ed96158
Martin <panovski.martin93@gmail.com>
2023-03-15 18:55:35 +0100

    Car exercise
    1) Car exercise added

---
+++ G1/Class06/SEDC.Class06/SEDC.Exercises/Models/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Exercise.Car.Models
+{
+    public class Driver
+    {
+        public Driver(string name, int level)
+        {
+            Name = name;
+            Level = level;
+        }
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
