3c10d10455214434df3e222b55f70bc9928b9918
trajanstevkovski <trajanstevkovski@gmail.com>
2023-03-18 11:37:01 +0100

    Class08 Linq

---
+++ G3/Class08/SEDC.Class08/SEDC.Class08.LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SEDC.Class08.LINQ;
+
+Console.WriteLine("Hello, World!");
+
+List<Dog> dogs = new List<Dog>()
+            {
+                new Dog(){Name = "Sparky", Age = 2},
+                new Dog(){Name = "Butch", Age = 1},
+                new Dog(){Name = "Zoe", Age = 3},
+                new Dog(){Name = "Sia", Age = 1},
+                new Dog(){Name = "William", Age = 4},
+                new Dog(){Name = "Billy", Age = 1},
+                new Dog(){Name = "Buck", Age = 2}
+            };
