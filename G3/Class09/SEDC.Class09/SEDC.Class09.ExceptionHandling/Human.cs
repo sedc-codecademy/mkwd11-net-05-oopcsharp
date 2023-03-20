8177ca68b02d72ed6fccae3e57b6445cc95cf3fb
trajanstevkovski <trajanstevkovski@gmail.com>
2023-03-20 20:39:36 +0100

    Class09 exceptions

---
+++ G3/Class09/SEDC.Class09/SEDC.Class09.ExceptionHandling/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Class09.ExceptionHandling
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
