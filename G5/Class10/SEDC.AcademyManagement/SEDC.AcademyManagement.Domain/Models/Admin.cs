1c245e14bf41c784ce60baaac4d40eeaf1638288
Angel Mitrov <Angel.Mitrov@seavus.com>
2023-03-22 19:12:42 +0100

    Partially Implemented Workshop

---
+++ G5/Class10/SEDC.AcademyManagement/SEDC.AcademyManagement.Domain/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using SEDC.AcademyManagement.Domain.Enums;
+
+namespace SEDC.AcademyManagement.Domain.Models
+{
+    public class Admin : User
+    {
+        public Admin(string username, string firstname, string lastname, int age) : base(username, firstname, lastname, age)
+        {
+            Role = Role.Admin;
+        }
+    }
+}
---
+++ G5/Class10/SEDC.AcademyManagement/SEDC.AcademyManagement.Domain/Models/Subject.cs
@@ -0,0 +1,12 @@
+﻿namespace SEDC.AcademyManagement.Domain.Models
+{
+    public class Subject
+    {
+        public Subject(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
