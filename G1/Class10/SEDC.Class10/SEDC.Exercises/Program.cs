3517f4e77ef4b39567c0abb3b7f0a6007a214da2
Martin <panovski.martin93@gmail.com>
2023-03-23 19:26:30 +0100

    Class 10 demo code

---
+++ G1/Class10/SEDC.Class10/SEDC.Exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿//1.Collect the inputs
+//Write a console application that will ask the user to
+//input 5 different type of inputs. If the user successfully
+//add the values, display them in the console,
+//else provide him an appropriate message thate he is doing something wrong.
+
+try
+{
+    Console.WriteLine("Please enter a message");
+    string userInputMessage = Console.ReadLine();
+
+    Console.WriteLine("Please enter a number");
+    int userInputNumber = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Please enter a character");
+    char userInputChar = char.Parse(Console.ReadLine());
+
+    Console.WriteLine("Please enter a decimal number");
+    double userInputDecimal = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Please enter boolean");
+    bool userInputBoolean = bool.Parse(Console.ReadLine());
+
+
+    Console.WriteLine($"You entered message: {userInputMessage}");
+    Console.WriteLine($"You entered number: {userInputNumber}");
+    Console.WriteLine($"You entered character: {userInputChar}");
+    Console.WriteLine($"You entered decimal number: {userInputDecimal}");
+    Console.WriteLine($"You entered boolean: {userInputBoolean}");
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("You entered a wrong input. Please try again!");
+}
