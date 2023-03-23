05e71722445b6ecda24f865d6b1644dd8ee86997
Martin <panovski.martin93@gmail.com>
2023-03-23 19:26:15 +0100

    Class 10 code

---
+++ G1/Class10/SEDC.Class10/SEDC.Exercises_2/Months.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Exercises_2
+{
+    public enum Months
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
