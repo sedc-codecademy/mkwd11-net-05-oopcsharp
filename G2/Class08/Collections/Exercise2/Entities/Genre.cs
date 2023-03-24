3e34df9b5d81476b3dbf4139eac120683ae460fe
petreArsovski <email@example.comarso.pet@outlook.com>
2023-03-24 21:36:04 +0100

    class 08

---
+++ G2/Class08/Collections/Exercise2/Entities/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2.Entities
+{
+    public enum Genre
+    {
+        Rock,
+        Hip_hop,
+        Techno,
+        Classical
+    }
+}
---
+++ G2/Class08/Collections/Exercise2/Entities/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<Song> FavouriteSongs { get; set; }
+
+        public Person()
+        {
+            FavouriteSongs = new List<Song>();
+        }
+
+        public void GetFavSongs()
+        {
+            if (FavouriteSongs.Count == 0)
+            {
+                Console.WriteLine("This person hates music!");
+            }
+            else
+            {
+                Console.WriteLine($"{FirstName} {LastName} favourite songs:");
+                foreach (Song song in FavouriteSongs)
+                {
+                    Console.WriteLine(song.Title);
+                }
+            }
+        }
+    }
+}
